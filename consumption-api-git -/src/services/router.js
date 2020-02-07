3b020645581d579ccc94e427197a085648d33ed5
Alexandro Messias <alexandrogts@hotmail.com>
2020-02-07 05:48:15 -0300

    Update router.js

--- consumption-api-git -/src/services/router.js
+++ consumption-api-git -/src/services/router.js
@@ -6,8 +6,8 @@ import Home from '../views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
+    mode:'history',
     routes:[
-        mode: 'history',
         {
             path: '*',
             name: 'home',
